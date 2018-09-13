fd1575ef14f6fa4d7b7b7429edaff9291b6306a5
RAHUL PAL <rahul.pal35@outlook.com>
2018-09-13 22:25:58 +0530

    commit after  setter injection with annotation.

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -9,16 +9,27 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
     private FortuneService fortuneService;
 
-    @Autowired
+    public TennisCoach() {
+        System.out.println("inside default constructor");
+    }
+/* @Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
-    }
+    }*/
+
+ /*   @Autowired
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("inside setFortuneService");
+
+        this.fortuneService = fortuneService;
+    }*/
 
     @Autowired
-    public void setFortuneService(FortuneService fortuneService) {
+    public void doSomeTest(FortuneService fortuneService) {
+        System.out.println("inside setFortuneService");
+
         this.fortuneService = fortuneService;
     }
-
     @Override
     public String getDailyWorkout() {
         return "Practice your backend volley";
