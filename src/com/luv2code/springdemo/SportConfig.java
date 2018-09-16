b69da548d12d35c165bcbbfe06fec15e8891e92a
RAHUL PAL <rahul.pal35@outlook.com>
2018-09-16 20:03:36 +0530

    Annotation final java Code

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,17 +1,21 @@
 package com.luv2code.springdemo;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
-
+//@ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
     @Bean
-    public Coach SwimCoach() {
-        SwimCoach() theCoach = new SwimCoach();
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    @Bean
+    public Coach swimCoach(FortuneService fortuneService) {
+        return new SwimCoach(sadFortuneService());
     }
 }
--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,11 +1,8 @@
 package com.luv2code.springdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SwimCoach implements Coach {
    /* @Autowired
     @Qualifier("happyFortuneService")
@@ -37,17 +34,29 @@ public class SwimCoach implements Coach {
     }*/
    private FortuneService fortuneService;
 
-    public SwimCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
+   @Value("${foo.email}")
+   private String email;
+   @Value("${foo.team}")
+    private String team;
+    public SwimCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
     }
 
     @Override
     public String getDailyWorkout() {
-        return "Swim 1000 meter";
+        return "Swim 1000 meters as a warm up.";
     }
 
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
     }
 }
